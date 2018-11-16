88d930b4ad77efbf964d502cd93b12bfa019e5d9
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2018-11-16 12:32:00 +0200

    fix places details

--- src/components/controls/DetailsField/index.jsx
+++ src/components/controls/DetailsField/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import { func, object, bool, string, oneOfType } from 'prop-types'
+import { func, array, object, bool, string, oneOfType } from 'prop-types'
 import {
-  Typography,
   IconButton,
   TextField,
   FormControl,
@@ -37,10 +36,11 @@ class DetailsField extends Component {
 
   add = () => {
     const { name, value, onChange } = this.props
-    onChange(name, [
-      ...value,
-      { label: this.label, value: this.value },
-    ])
+    const result = [...value, { label: this.label.value, value: this.value.value }]
+    this.label.value = ''
+    this.value.value = ''
+
+    onChange(name, result)
   }
 
   change = detail => (e) => {
@@ -48,8 +48,27 @@ class DetailsField extends Component {
 
     const valueIndex = value.findIndex(d => d.label === detail.label)
 
-    value[valueIndex] = { label: detail.label, value: e.target.value }
+    switch (e.target.name) {
+
+      case 'label':
+        value[valueIndex] = { label: e.target.value, value: detail.value }
+        break
+      case 'value':
+        value[valueIndex] = { label: detail.label, value: e.target.value }
+        break
+
+      default:
+        break
+    }
+
     onChange(name, [...value])
+  }
+
+  remove = detail => () => {
+    const { name, value, onChange } = this.props
+    const result = value.filter(d => d.label !== detail.label)
+
+    onChange(name, result)
   }
 
   render() {
@@ -60,35 +79,47 @@ class DetailsField extends Component {
         <FormLabel className={classes.label} component="legend">{label}</FormLabel>
         <table style={{ width: '100%' }}>
           <tbody>
-          {value.map(detail => (
+            {value.map((detail, index) => (
+              <tr key={index} className={classes.field}>
+                <td>
+                  <div className={classes.labelTd}>
+                    <TextField
+                      fullWidth
+                      name="label"
+                      className={classes.fieldValue}
+                      value={detail.label}
+                      onChange={this.change(detail)}
+                    />
+                  </div>
+                </td>
+                <td width="100%">
+                  <div className={classes.valueTd}>
+                    <TextField
+                      fullWidth
+                      name="value"
+                      className={classes.fieldValue}
+                      value={detail.value}
+                      onChange={this.change(detail)}
+                    />
+                  </div>
+                </td>
+                <td align="right"><IconButton onClick={this.remove(detail)}><DeleteIcon /></IconButton></td>
+
+              </tr>
+            ))}
             <tr className={classes.field}>
               <td>
                 <div className={classes.labelTd}>
-                  <Typography className={classes.fieldLabel}>{detail.label}:</Typography>
+                  <TextField fullWidth placeholder="Надпись" inputRef={(l) => { this.label = l }} />
                 </div>
               </td>
               <td width="100%">
                 <div className={classes.valueTd}>
-                  <TextField className={classes.fieldValue} value={detail.value} onChange={this.change(detail)} />
+                  <TextField fullWidth placeholder="Значение" inputRef={(v) => { this.value = v }} />
                 </div>
               </td>
-              <td align="right"><IconButton onClick={this.add}><DeleteIcon /></IconButton></td>
-
+              <td><IconButton onClick={this.add}><PlusIcon /></IconButton></td>
             </tr>
-          ))}
-          <tr className={classes.field}>
-            <td>
-              <div className={classes.labelTd}>
-                <TextField placeholder="Надпись" innerRef={(l) => { this.label = l }} />
-              </div>
-            </td>
-            <td>
-              <div className={classes.valueTd}>
-                <TextField placeholder="Значение" innerRef={(v) => { this.value = v }} />
-              </div>
-            </td>
-            <td><IconButton onClick={this.add}><PlusIcon /></IconButton></td>
-          </tr>
           </tbody>
         </table>
 
@@ -101,7 +132,7 @@ class DetailsField extends Component {
 DetailsField.propTypes = {
   classes: object.isRequired,
   name: string.isRequired,
-  value: string.isRequired,
+  value: array.isRequired,
   onChange: func.isRequired,
   label: string,
   error: bool,
